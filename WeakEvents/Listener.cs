7eae5c6d588de3ac522ae6e9d393d2e899378e01
sericaer <sericaer@outlook.com>
2022-07-18 14:12:25 +0800

    Add WeakEvents

---
+++ WeakEvents/Listener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+
+namespace WeakEvents
+{
+    public class Listener<T>
+    {
+        public bool isAlive => weakReference.IsAlive;
+
+
+        private WeakReference weakReference;
+        private MethodInfo method;
+
+        public Listener(Action<T> action)
+        {
+            weakReference = new WeakReference(action.Target);
+            method = action.Method;
+        }
+
+        public void Raise(params object[] datas)
+        {
+            method.Invoke(weakReference.Target, datas);
+        }
+    }
+}
---
+++ WeakEvents/Publisher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WeakEvents
+{
+    public class Publisher<T>
+    {
+        public IEnumerable<Listener<T>> listeners => _listeners;
+
+        private List<Listener<T>> _listeners = new List<Listener<T>>();
+
+        public void AddListener(Action<T> action)
+        {
+            _listeners.Add(new Listener<T>(action));
+        }
+
+        public void Raise(T data)
+        {
+            var index = 0;
+            while (index < _listeners.Count)
+            {
+                var curr = _listeners[index];
+                if (curr.isAlive)
+                {
+                    curr.Raise(data);
+                    index++;
+                }
+                else
+                {
+                    _listeners.RemoveAt(index);
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            _listeners.Clear();
+        }
+    }
+}
